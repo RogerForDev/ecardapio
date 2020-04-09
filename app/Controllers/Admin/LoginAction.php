31503161e5e3afb0b7694c11c9a5b5fd203a2fdf
Vitor Baldacin <vitormatheussb@gmail.com>
2020-04-09 14:22:50 -0300

    correcao cadastro home

--- app/Controllers/Admin/LoginAction.php
+++ app/Controllers/Admin/LoginAction.php
@@ -63,7 +63,7 @@ final class LoginAction extends Controller{
             throw new \Exception("Usuário inexistente ou senha inválida.1");
         }
 
-        if(password_verify($password, $data["senha"]) === true)
+        if(password_verify($password, $data["senha"]) === false)
         {
             $_SESSION[User::SESSION] = $data;
         } else {
