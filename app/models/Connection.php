8f34e7aed9f23e5021e4ab494e00f9240841ebaf
rogerfordev <rogmtsi.df@gmail.com>
2020-04-09 14:25:35 -0300

    gitigone connection

--- app/models/Connection.php
+++ app/models/Connection.php
@@ -10,7 +10,7 @@ class Connection extends Controller {
 
 	public static function connect() {
 
-		$pdo = new PDO("mysql:host=localhost;dbname=ecardapio;charset=utf8", "root","");
+		$pdo = new PDO("mysql:host=localhost;dbname=ecardapio;charset=utf8", "root","bancodedados");
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 
