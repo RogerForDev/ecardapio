1041ec79756b9525905fc228e5c28ece96236bb7
rogerfordev <rogmtsi.df@gmail.com>
2020-05-26 15:27:50 -0300

    exclusão do produto

---
+++ assets/admin/js/cardapio.js
@@ -0,0 +1,21 @@
+function remove(url){
+    Swal.fire({
+        title: 'Tem certeza?',
+        text: "Você não poderá reverter isso!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        cancelButtonText: 'Não! deixa quieto',
+        confirmButtonText: 'Sim, exclua isso!'
+      }).then((result) => {
+        if (result.value) {
+          Swal.fire(
+            'Excluido!',
+            'Esse item foi excluido.',
+            'success'
+          )
+          window.location.href = url; 
+        }
+      });
+}
--- produtos.sql
+++ produtos.sql
@@ -11,123 +11,6 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
--- Copiando estrutura para tabela ecardapio.tb_cardapio
-CREATE TABLE IF NOT EXISTS `tb_cardapio` (
-  `id_cardapio` int(11) NOT NULL AUTO_INCREMENT,
-  `id_tema` int(11) NOT NULL DEFAULT '0',
-  `id_usuario` int(11) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id_cardapio`),
-  KEY `id_tema` (`id_tema`),
-  KEY `id_usuario` (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- Copiando dados para a tabela ecardapio.tb_cardapio: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `tb_cardapio` DISABLE KEYS */;
-INSERT INTO `tb_cardapio` (`id_cardapio`, `id_tema`, `id_usuario`) VALUES
-	(1, 1, 1);
-/*!40000 ALTER TABLE `tb_cardapio` ENABLE KEYS */;
-
--- Copiando estrutura para tabela ecardapio.tb_cardapio_produto
-CREATE TABLE IF NOT EXISTS `tb_cardapio_produto` (
-  `id_cardapio` int(11) DEFAULT NULL,
-  `id_produto` int(11) DEFAULT NULL,
-  KEY `id_cardapio` (`id_cardapio`),
-  KEY `id_produto` (`id_produto`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Copiando dados para a tabela ecardapio.tb_cardapio_produto: ~54 rows (aproximadamente)
-/*!40000 ALTER TABLE `tb_cardapio_produto` DISABLE KEYS */;
-INSERT INTO `tb_cardapio_produto` (`id_cardapio`, `id_produto`) VALUES
-	(1, 1),
-	(1, 3),
-	(1, 4),
-	(1, 5),
-	(1, 6),
-	(1, 40),
-	(1, 7),
-	(1, 8),
-	(1, 9),
-	(1, 10),
-	(1, 11),
-	(1, 12),
-	(1, 13),
-	(1, 14),
-	(1, 15),
-	(1, 16),
-	(1, 17),
-	(1, 18),
-	(1, 19),
-	(1, 20),
-	(1, 21),
-	(1, 22),
-	(1, 23),
-	(1, 24),
-	(1, 25),
-	(1, 26),
-	(1, 27),
-	(1, 28),
-	(1, 29),
-	(1, 30),
-	(1, 31),
-	(1, 32),
-	(1, 33),
-	(1, 34),
-	(1, 35),
-	(1, 36),
-	(1, 37),
-	(1, 38),
-	(1, 39),
-	(1, 41),
-	(1, 42),
-	(1, 43),
-	(1, 44),
-	(1, 45),
-	(1, 46),
-	(1, 47),
-	(1, 48),
-	(1, 49),
-	(1, 50),
-	(1, 51),
-	(1, 52),
-	(1, 53),
-	(1, 54),
-	(1, 55),
-	(1, 56),
-	(1, 57),
-	(1, 58),
-	(1, 59),
-	(1, 60),
-	(1, 61),
-	(1, 62),
-	(1, 63),
-	(1, 64);
-/*!40000 ALTER TABLE `tb_cardapio_produto` ENABLE KEYS */;
-
--- Copiando estrutura para tabela ecardapio.tb_categoria
-CREATE TABLE IF NOT EXISTS `tb_categoria` (
-  `id_categoria` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(55) NOT NULL,
-  `icone` varchar(55) DEFAULT NULL,
-  `imagem` varchar(255) DEFAULT NULL,
-  `ativo` tinyint(4) NOT NULL DEFAULT '1',
-  `id_cardapio` int(11) NOT NULL,
-  PRIMARY KEY (`id_categoria`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=latin1;
-
--- Copiando dados para a tabela ecardapio.tb_categoria: ~8 rows (aproximadamente)
-/*!40000 ALTER TABLE `tb_categoria` DISABLE KEYS */;
-INSERT INTO `tb_categoria` (`id_categoria`, `nome`, `icone`, `imagem`, `ativo`, `id_cardapio`) VALUES
-	(1, 'Lanches', NULL, NULL, 1, 0),
-	(2, 'Bebidas', NULL, NULL, 1, 0),
-	(3, 'Crepe Suiço', NULL, NULL, 1, 0),
-	(4, 'Tapioca Salgada', NULL, NULL, 1, 0),
-	(11, 'Tapioca doce', NULL, NULL, 1, 1),
-	(12, 'Açaí', NULL, NULL, 1, 1),
-	(16, 'Roger', NULL, 'C:/laragon/www/ecardapio/uploads/images/2020/05/26/15904516355ecc5db370e65/arquivo5becbd4d1e3034701508400322761f81.jpg', 1, 1),
-	(17, 'Teste2', NULL, 'C:/laragon/www/ecardapio/uploads/categoria/2020/05/26/15904519385ecc5ee292346/arquivod8d2199b599b1300f3462f39647c703a.jpg', 1, 1),
-	(18, 'Roger2', NULL, 'C:/laragon/www/ecardapio/uploads/categoria/2020/05/26/15904526435ecc61a34a7f0/arquivo029884db58539d4fa9baad74aa9b1709.jpg', 1, 1);
-/*!40000 ALTER TABLE `tb_categoria` ENABLE KEYS */;
-
 -- Copiando estrutura para tabela ecardapio.tb_produto
 CREATE TABLE IF NOT EXISTS `tb_produto` (
   `id_produto` int(11) NOT NULL AUTO_INCREMENT,
@@ -141,13 +24,13 @@ CREATE TABLE IF NOT EXISTS `tb_produto` (
   KEY `id_categoria` (`id_categoria`)
 ) ENGINE=InnoDB AUTO_INCREMENT=65 DEFAULT CHARSET=utf8;
 
--- Copiando dados para a tabela ecardapio.tb_produto: ~55 rows (aproximadamente)
+-- Copiando dados para a tabela ecardapio.tb_produto: ~53 rows (aproximadamente)
 /*!40000 ALTER TABLE `tb_produto` DISABLE KEYS */;
 INSERT INTO `tb_produto` (`id_produto`, `nome`, `descricao`, `id_categoria`, `imagem`, `preco`, `ativo`) VALUES
 	(1, 'X-Salada', 'Hambúrguer - Maionese - Alface - Tomate Milho - Queijo e Presunto - Batata-palha', 1, 'uploads/produto/2020/05/26/15904529895ecc62fd01185/arquivo207209ca59b97766e744782e819e8e2e.jpg', 7.50, 1),
 	(2, 'X – EGG', 'Hambúrguer - Maionese - Alface – Tomate - Milho - Queijo e Presunto - Batata-palha – Ovo Frito', 1, 'uploads/produto/2020/05/26/15905107295ecd448904e66/arquivo21fa1227b45a89babcdabb9d4f9c8ea0.png', 8.00, 1),
 	(3, 'HOT-DOG PRENSADO', 'Salsicha Defumada - Maionese - Alface - Tomate - Milho - Batata-palha – Catchup', 1, 'uploads/produto/2020/05/26/64c3fa6d31a69abc2b4f530c70026517.jpg', 8.00, 1),
-	(4, 'X – BACON', 'Hambúrguer - Maionese - Alface – Tomate Milho - Queijo e Presunto – Bacon - Batata-palha', 1, 'uploads/produto/2020/05/26/b98d3621df9cec0886f22baaf40eb9b7.jpg', 9.00, 1),
+	(4, 'X – BACON', 'Hambúrguer - Maionese - Alface – Tomate Milho - Queijo e Presunto – Bacon - Batata-palha', 1, '', 9.00, 1),
 	(5, 'X – CALABRESA', 'Hambúrguer – Maionese - Alface Batata-palha - Tomate - Milho - Queijo e Presunto –Calabresa', 1, '', 9.00, 1),
 	(6, 'X – FRANGO', 'Hambúrguer – Filé de Frango - Maionese Alface - Tomate - Milho - Queijo e Presunto', 1, '', 12.00, 1),
 	(7, 'X – TUDO', 'Hambúrguer - Maionese - Alface - Tomate Milho - Queijo e Presunto - Batata-palha – Ovo Frito - Calabresa – Bacon', 1, '', 15.00, 1),
@@ -200,53 +83,6 @@ INSERT INTO `tb_produto` (`id_produto`, `nome`, `descricao`, `id_categoria`, `im
 	(63, 'asasas', 'asasas', 1, 'uploads/produto/2020/05/26/18eaa731540b41a262a912bfc3cd403f.jpg', 0.00, 1),
 	(64, 'ccacaca', 'cacac', 1, 'uploads/produto/2020/05/26/e4ee86a26ac107a4e580535ddc5f3260.jpg', 0.00, 1);
 /*!40000 ALTER TABLE `tb_produto` ENABLE KEYS */;
-
--- Copiando estrutura para tabela ecardapio.tb_tema
-CREATE TABLE IF NOT EXISTS `tb_tema` (
-  `id_tema` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(50) DEFAULT NULL,
-  `cor_primaria` varchar(50) DEFAULT NULL,
-  `background` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id_tema`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
-
--- Copiando dados para a tabela ecardapio.tb_tema: ~0 rows (aproximadamente)
-/*!40000 ALTER TABLE `tb_tema` DISABLE KEYS */;
-INSERT INTO `tb_tema` (`id_tema`, `nome`, `cor_primaria`, `background`) VALUES
-	(1, 'padrao', NULL, NULL);
-/*!40000 ALTER TABLE `tb_tema` ENABLE KEYS */;
-
--- Copiando estrutura para tabela ecardapio.tb_usuario
-CREATE TABLE IF NOT EXISTS `tb_usuario` (
-  `id_usuario` int(11) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(55) NOT NULL,
-  `login` varchar(55) NOT NULL,
-  `senha` varchar(255) DEFAULT NULL,
-  `email` varchar(55) DEFAULT NULL,
-  `ativo` tinyint(4) DEFAULT '1',
-  `estabelecimento` varchar(55) DEFAULT NULL,
-  `horario` varchar(55) DEFAULT NULL,
-  `sobre` text,
-  `categoria` varchar(55) DEFAULT NULL,
-  `cidade` varchar(55) DEFAULT NULL,
-  `rua` varchar(55) DEFAULT NULL,
-  `bairro` varchar(55) DEFAULT NULL,
-  `numero` varchar(55) DEFAULT NULL,
-  `telefone` varchar(55) DEFAULT NULL,
-  `whatsapp` varchar(55) DEFAULT NULL,
-  `facebook` varchar(55) DEFAULT NULL,
-  `instagram` varchar(55) DEFAULT NULL,
-  `flag_cartao` tinyint(4) DEFAULT NULL,
-  `flag_wifi` tinyint(4) DEFAULT NULL,
-  PRIMARY KEY (`id_usuario`)
-) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
-
--- Copiando dados para a tabela ecardapio.tb_usuario: ~2 rows (aproximadamente)
-/*!40000 ALTER TABLE `tb_usuario` DISABLE KEYS */;
-INSERT INTO `tb_usuario` (`id_usuario`, `nome`, `login`, `senha`, `email`, `ativo`, `estabelecimento`, `horario`, `sobre`, `categoria`, `cidade`, `rua`, `bairro`, `numero`, `telefone`, `whatsapp`, `facebook`, `instagram`, `flag_cartao`, `flag_wifi`) VALUES
-	(1, 'Administrador', 'admin', '$2y$12$58zQvfbvUSWYtfL.y5VR7eyvQkX6E7Y3cML91nvIgLzRMTyeFJtQy', 'admin@mail.com', 1, 'Tapioca Ceara', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
-	(11, 'Teste2', 'teste', '$2y$12$Qs0O7oDCnv083zaITSEZ..0P48GIjVn6fG7dFnjke7Av0LbpfnA..', 'teste@teste.com', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
-/*!40000 ALTER TABLE `tb_usuario` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
