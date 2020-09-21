9c7ac0354af6dc864b232944c5237fa9343395e7
Vitor Baldacin <vitormatheussb@gmail.com>
2020-09-21 16:39:38 -0300

    correção bugs facebook e mascaras

--- assets/admin/js/cardapio.js
+++ assets/admin/js/cardapio.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
   $('#altera-telefone').mask('(99)0000-0000', {reverse: false});
   $('#altera-whats').mask('(99)99999-9999', {reverse: false});
-  $('#preco-produto-cadastro').mask('999.999,99', {reverse: true});
+  $('.preco-produto-cadastro').mask('999.999,99', {reverse: true});
   // $('#altera-horario').mask('00:00:00', {reverse: true});
   
 });
